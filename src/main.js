c5396972bdd27a16be5b7d5e8d3c648c72622099
Rakel Moreira <rakelmoreiramendonca@gmail.com>
2022-10-31 15:09:20 -0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import "./css/index.css"
+
+const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path");
+const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path");
+
+
+
+
+function setCardType(type) {
+    const colors = {
+        "visa": ["#436D99","#2D57F2"],
+        "mastercard":  ["#C69347", "#DF6F29"],
+        "default": ["black", "gray"],
+    }
+    
+    ccBgColor01.setAttribute("fill", colors[type] [0])
+    ccBgColor02.setAttribute("fill", colors[type] [1])
+}
+setCardType("default")
